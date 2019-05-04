74638d6ddcf4ff7a29c14f80ec89453c92ce597b
dlevenson44 <daniellevenson4@gmail.com>
2019-05-04 16:35:19 -0400

    Add images to Floors and Remodel components

--- src/components/Floors.jsx
+++ src/components/Floors.jsx
@@ -1,10 +1,50 @@
 import React from 'react';
 import '../App.css';
 
+import Image1 from '../imgs/flooring/flooring3.jpg';
+import Image2 from '../imgs/flooring/flooring1.jpg';
+import Image3 from '../imgs/flooring/flooring6.jpg';
+import Image4 from '../imgs/flooring/flooring4.jpg';
+import Image5 from '../imgs/flooring/flooring2.jpg';
+import Image6 from '../imgs/flooring/flooring5.jpg';
+
 function Floors() {
   return (
     <div className="section-container">
-      Hello Floors
+      <div className="row">
+        <img
+          className="small-grid-img"
+          alt="worcester-county-carpentry-floors"
+          src={Image1}
+        />
+        <img
+          className="small-grid-img"
+          alt="worcester-county-carpentry-floors"
+          src={Image2}
+        />
+        <img
+          className="small-grid-img"
+          alt="worcester-county-carpentry-floors"
+          src={Image3}
+        />
+      </div>
+      <div className="row">
+        <img
+          className="small-grid-img"
+          alt="worcester-county-carpentry-floors"
+          src={Image4}
+        />
+        <img
+          className="small-grid-img"
+          alt="worcester-county-carpentry-floors"
+          src={Image5}
+        />
+        <img
+          className="small-grid-img"
+          alt="worcester-county-carpentry-floors"
+          src={Image6}
+        />
+      </div>
     </div>
   );
 }
--- src/components/Remodel.jsx
+++ src/components/Remodel.jsx
@@ -1,10 +1,50 @@
 import React from 'react';
 import '../App.css';
 
+import Image1 from '../imgs/remodel/remodel7.jpg';
+import Image2 from '../imgs/restoration/restoration_1.JPG';
+import Image3 from '../imgs/remodel/remodel11.jpg';
+import Image4 from '../imgs/restoration/restoration_2.JPG';
+import Image5 from '../imgs/remodel/remodel9.jpg';
+import Image6 from '../imgs/remodel/remodel3.jpg';
+
 function Remodel() {
   return (
     <div className="section-container">
-      Hello Remodel
+      <div className="row">
+        <img
+          className="small-grid-img"
+          alt="worcester-county-carpentry-remodeling"
+          src={Image1}
+        />
+        <img
+          className="small-grid-img"
+          alt="worcester-county-carpentry-remodeling"
+          src={Image2}
+        />
+        <img
+          className="small-grid-img"
+          alt="worcester-county-carpentry-remodeling"
+          src={Image3}
+        />
+      </div>
+      <div className="row">
+        <img
+          className="small-grid-img"
+          alt="worcester-county-carpentry-remodeling"
+          src={Image4}
+        />
+        <img
+          className="small-grid-img"
+          alt="worcester-county-carpentry-remodeling"
+          src={Image5}
+        />
+        <img
+          className="small-grid-img"
+          alt="worcester-county-carpentry-remodeling"
+          src={Image6}
+        />
+      </div>
     </div>
   );
 }
