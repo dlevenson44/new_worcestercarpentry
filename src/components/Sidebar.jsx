e292e548feffc6db136bf195ceb9aa950542e726
dlevenson44 <daniellevenson4@gmail.com>
2019-05-04 12:31:39 -0400

    Styled Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,6 +7,18 @@ function Sidebar() {
   return (
     <div className="sidebar-container">
       <img src={Logo} className="logo" alt="worcester-county-carpentry-logo" />
+      <h1 className="sidebar-header">Worcester County</h1>
+      <h1 className="sidebar-header" id="header2">Carpentry</h1>
+      <h3 className="sidebar-subheader">Over 15 Years Experience</h3>     
+      <p class="sidebar-text">
+        Next time you're thinking of having work done to your home give us a call.  Whether it's an addition, remodel, or a restoration, we will get the job done right for you.
+      </p>
+      <p class="sidebar-text">    
+        Call us today:  <a class="sidebar-link" href="tel:508-523-6797">508-523-6797</a>
+      </p>
+      <p class="sidebar-text">
+        Need masonry work done? <a class="sidebar-link" href="http://worcesterchimneys.com/">Click here!</a>
+      </p>
     </div>
   );
 }
