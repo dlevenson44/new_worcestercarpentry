e873491e8780ef77e3a1f900d6cbb92f8156cd3e
dlevenson44 <daniellevenson4@gmail.com>
2019-05-04 15:14:10 -0400

    updated nav bar with styling

--- src/components/Blurb.jsx
+++ src/components/Blurb.jsx
@@ -5,8 +5,28 @@ function Blurb() {
   return (
     <div className="blurb-container">
       <h5 className="blurb-text">
-        At Worcester County Carpentry, our top priority is doing the right job for YOU.  With top notch customer service, we will work with you to make sure that every detail of our work is up to your liking and our standards.  With over 15 years experience of building, repairing, and remodeling, we do work on decks, floors, siding, windows, doors.
+        At Worcester County Carpentry, our top priority is doing the right job for you.  With top notch customer service, we will work with you to make sure that every detail of our work is up to your liking and our standards.  With over 15 years experience of building, repairing, and remodeling, we do work on decks, floors, siding, windows, doors.
       </h5>
+      <ul className="nav-bar">
+        <li className="nav-item" onClick={() => console.log('clicked')}>
+          Decks
+        </li>
+        <li className="nav-item" onClick={() => console.log('clicked')}>
+          Floors
+        </li>
+        <li className="nav-item" onClick={() => console.log('clicked')}>
+          Remodeling & Restoration
+        </li>
+        <li className="nav-item" onClick={() => console.log('clicked')}>
+          Roofing
+        </li>
+        <li className="nav-item" onClick={() => console.log('clicked')}>
+          Siding
+        </li>
+        <li className="nav-item" onClick={() => console.log('clicked')}>
+          Windows & Doors
+        </li>
+      </ul>
     </div>
   );
 }
