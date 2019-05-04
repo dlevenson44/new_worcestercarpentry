11ca240e865f0c831529dd0386edddb1d4d5903c
dlevenson44 <daniellevenson4@gmail.com>
2019-05-04 16:24:54 -0400

    Add grid images for Decks

--- src/components/Decks.jsx
+++ src/components/Decks.jsx
@@ -1,10 +1,50 @@
 import React from 'react';
 import '../App.css';
 
+import Image1 from '../imgs/decksporches/customdeckstairs1.jpg';
+import Image2 from '../imgs/decksporches/decksporches4.jpg';
+import Image3 from '../imgs/decksporches/decksporches9.jpg';
+import Image4 from '../imgs/decksporches/decksporches6.jpg';
+import Image5 from '../imgs/decksporches/decksporches2.jpg';
+import Image6 from '../imgs/decksporches/decksporches7.jpg';
+
 function Decks() {
   return (
     <div className="section-container">
-      Hello Decks
+      <div className="row">
+        <img
+          className="small-grid-img"
+          alt="worcester-county-carpentry-decks"
+          src={Image1}
+        />
+        <img
+          className="small-grid-img"
+          alt="worcester-county-carpentry-decks"
+          src={Image2}
+        />
+        <img
+          className="small-grid-img"
+          alt="worcester-county-carpentry-decks"
+          src={Image3}
+        />
+      </div>
+      <div className="row">
+        <img
+          className="small-grid-img"
+          alt="worcester-county-carpentry-decks"
+          src={Image4}
+        />
+        <img
+          className="small-grid-img"
+          alt="worcester-county-carpentry-decks"
+          src={Image5}
+        />
+        <img
+          className="small-grid-img"
+          alt="worcester-county-carpentry-decks"
+          src={Image6}
+        />
+      </div>
     </div>
   );
 }
