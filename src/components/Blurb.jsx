4f1c3930144f08dad805dc9653c7b210c19deb7b
dlevenson44 <daniellevenson4@gmail.com>
2019-05-04 15:30:55 -0400

    Update Blurb to handle pageSelector prop

--- src/components/Blurb.jsx
+++ src/components/Blurb.jsx
@@ -1,29 +1,48 @@
 import React from 'react';
 import '../App.css';
 
-function Blurb() {
+function Blurb(props) {
+  console.log('props:', props)
   return (
     <div className="blurb-container">
       <h5 className="blurb-text">
         At Worcester County Carpentry, our top priority is doing the right job for you.  With top notch customer service, we will work with you to make sure that every detail of our work is up to your liking and our standards.  With over 15 years experience of building, repairing, and remodeling, we do work on decks, floors, siding, windows, doors.
       </h5>
       <ul className="nav-bar">
-        <li className="nav-item" onClick={() => console.log('clicked')}>
+        <li
+          className="nav-item"
+          onClick={(e) => props.pageSelector(e, 'decks')}
+        >
           Decks
         </li>
-        <li className="nav-item" onClick={() => console.log('clicked')}>
+        <li
+          className="nav-item"
+          onClick={(e) => props.pageSelector(e, 'floors')}
+        >
           Floors
         </li>
-        <li className="nav-item" onClick={() => console.log('clicked')}>
+        <li
+          className="nav-item"
+          onClick={(e) => props.pageSelector(e, 'remodeling')}
+        >
           Remodeling & Restoration
         </li>
-        <li className="nav-item" onClick={() => console.log('clicked')}>
+        <li
+          className="nav-item"
+          onClick={(e) => props.pageSelector(e, 'roofing')}
+        >
           Roofing
         </li>
-        <li className="nav-item" onClick={() => console.log('clicked')}>
+        <li
+          className="nav-item"
+          onClick={(e) => props.pageSelector(e, 'siding')}
+        >
           Siding
         </li>
-        <li className="nav-item" onClick={() => console.log('clicked')}>
+        <li
+          className="nav-item"
+          onClick={(e) => props.pageSelector(e, 'windows')}
+        >
           Windows & Doors
         </li>
       </ul>
